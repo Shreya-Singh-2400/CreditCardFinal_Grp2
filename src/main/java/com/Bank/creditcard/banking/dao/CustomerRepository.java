0711a18f6b2adba5b5eea665da976909a9718c28
Shreya Singh <singhshreya2400@gmail.com>
2023-08-17 17:14:18 +0000

    final commit

---
+++ src/main/java/com/Bank/creditcard/banking/dao/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.Bank.creditcard.banking.dao;
+
+import com.Bank.creditcard.banking.entity.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+    List<Customer> findAllByCustomerId(long customerId);
+    List<Customer> findCustomersByJob(String job);
+public boolean existsByCustomerId(long id);
+
+}
